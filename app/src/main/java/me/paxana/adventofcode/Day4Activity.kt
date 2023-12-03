39c51714ca7557f4697cc2ccde82f771a07b2029
paxblueribbon <veganbikepunk@gmail.com>
2023-12-02 20:31:34 -0800

    Minor refactor to remove warning

---
+++ app/src/main/java/me/paxana/adventofcode/Day4Activity.kt
@@ -0,0 +1,59 @@
+package me.paxana.adventofcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+
+class Day4Activity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_day3)
+    Logger.addLogAdapter(AndroidLogAdapter())
+    val powerList = mutableListOf<Int>()
+
+    assets.open("Day2Input.txt").bufferedReader().useLines { lines ->
+      lines.forEach { lineStr ->
+        val results = mutableMapOf(
+          "red" to mutableListOf<Int>(),
+          "green" to mutableListOf(),
+          "blue" to mutableListOf()
+        )
+
+        val withoutTitle = lineStr.split(":")[1]
+        val replaceSemicolonsSplitCommasTrimSpaces = withoutTitle.replace(";", ",").split(",").map{ it.trim() }
+
+        replaceSemicolonsSplitCommasTrimSpaces.forEach { cubePull ->
+
+          val split = cubePull.split(" ")
+          when (split[1]) {
+            "green" -> results["green"]!!.add(split[0].toInt())
+            "red" -> results["red"]!!.add(split[0].toInt())
+            "blue" -> results["blue"]!!.add(split[0].toInt())
+          }
+        }
+
+        val justMaxes = maxes(results)
+        val maxesPower = (justMaxes["red"]!! * justMaxes["green"]!! * justMaxes["blue"]!!)
+
+        powerList.add(maxesPower)
+      }
+      Logger.d("Power List sum: ${powerList.sum()}")
+    }
+  }
+  private fun maxes(results: MutableMap<String, MutableList<Int>>): Map<String, Int> {
+    val red = results["red"]!!
+    val green = results["green"]!!
+    val blue = results["blue"]!!
+
+    val redMax = red.maxOrNull()!!
+    val greenMax = green.maxOrNull()!!
+    val blueMax = blue.maxOrNull()!!
+
+    return mapOf(
+      "red" to redMax,
+      "green" to greenMax,
+      "blue" to blueMax
+    )
+  }
+}
