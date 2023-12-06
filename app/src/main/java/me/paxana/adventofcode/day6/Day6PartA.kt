d2ffa03b33d4f8650d0e795862446f91d4338bbf
paxblueribbon <veganbikepunk@gmail.com>
2023-12-06 12:28:08 -0800

    Adding day 6 part A files

---
+++ app/src/main/java/me/paxana/adventofcode/day6/Day6PartA.kt
@@ -0,0 +1,14 @@
+package me.paxana.adventofcode.day6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.paxana.adventofcode.R
+
+class Day6PartA : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_day6_parta)
+
+
+  }
+}
